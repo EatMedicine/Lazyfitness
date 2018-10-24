eaa9973e9a66519c525b5a35a10bc08c22930ee3
EatMedicine <870830676@qq.com>
2018-10-24 23:01:53 +0800

    添加了一个引用 微小的修改

--- Lazyfitness/Models/userSecurity.cs
+++ Lazyfitness/Models/userSecurity.cs
@@ -11,7 +11,8 @@ namespace Lazyfitness.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class userSecurity
     {
         public int userId { get; set; }
