75aa22404f69a14bc903433f1c6d1673438bcfe7
Oliver Queen <1092519577@qq.com>
2018-10-29 11:28:58 +0800

    后台除统计信息外完毕，验证信息需要security这张表加一个密保信息的验证。

--- Lazyfitness/Areas/backStage/Controllers/payManagementController.cs
+++ Lazyfitness/Areas/backStage/Controllers/payManagementController.cs
@@ -24,13 +24,6 @@ namespace Lazyfitness.Areas.backStage.Controllers
                 return View();
             }
         }
-        #endregion
-
-        #region 充值卡管理
-        public ActionResult refillCardManagement()
-        {
-            return View();
-        }
         #region 查询用户
         [HttpPost]
         public ActionResult findUser(string userName)
@@ -59,6 +52,80 @@ namespace Lazyfitness.Areas.backStage.Controllers
                 Info.userAccount = userAccount;
                 db.SaveChanges();
                 return "修改成功";
+            }
+        }
+        #endregion
+        #region 账户清零
+        [HttpPost]
+        public string resetAccount(int userId)
+        {
+            using (LazyfitnessEntities db = new LazyfitnessEntities())
+            {
+                var allInfo = db.userInfo.Where(u => u.userId == userId);
+                userInfo obInfo = allInfo.FirstOrDefault();
+                obInfo.userAccount = 0;
+                db.SaveChanges();
+                return "success";
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region 充值卡管理
+        public ActionResult refillCardManagement()
+        {
+            using (LazyfitnessEntities db = new LazyfitnessEntities())
+            {
+                var allList = db.recharge.ToList();
+                ViewBag.allList = allList;
+                return View();
+            }
+        }
+        #region 查看卡号
+        public ActionResult findCardId()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult findCardId(string rechargeId)
+        {
+            using (LazyfitnessEntities db = new LazyfitnessEntities())
+            {
+                var allInfo = db.recharge.Where(u => u.rechargeId == rechargeId.Trim());
+                if (allInfo.ToList().Count == 0)
+                {
+                    return Content("没有此卡");
+                }
+                ViewBag.allInfo = allInfo.FirstOrDefault();
+            }
+            return View();
+        }
+        #endregion
+        #region 更改充值卡信息
+        public ActionResult changeCardInfo(recharge charge)
+        {
+            using (LazyfitnessEntities db = new LazyfitnessEntities())
+            {
+                var dbCharge = db.recharge.Where(u => u.rechargeId == charge.rechargeId.Trim());
+                recharge obCharge = dbCharge.FirstOrDefault();
+                obCharge.rechargePwd = charge.rechargePwd;
+                obCharge.amount = charge.amount;
+                obCharge.isAvailable = charge.isAvailable;
+                db.SaveChanges();
+                return Content("修改成功");
+            }
+        }
+        #endregion
+        #region 清空不可用卡号
+        public string resetUnAvailable()
+        {
+            using (LazyfitnessEntities db = new LazyfitnessEntities())
+            {
+                var deleteList = db.recharge.Where(u => u.isAvailable == 0).ToList();
+                db.recharge.RemoveRange(deleteList);
+                db.SaveChanges();
+                return "success";
             }
         }
         #endregion
--- Lazyfitness/Areas/jQuery/articleManagement.js
+++ Lazyfitness/Areas/jQuery/articleManagement.js
@@ -3,14 +3,13 @@ $(function () {
     $(".deleteArticle").click(function () {
         //获取所点击删除按钮的value值
         var getArticleId = $(this).val();
-        alert(getArticleId);
         //把这个值发送到后台       
         $.post("/backStage/articleManagement/deleteArticle", "resourceId=" + getArticleId, function (data, status) {
             if (data == "success" && status == "success") {
                 alert("删除成功!");
-            }
-            //刷新当前页面
-            window.location.reload();
+                //刷新当前页面
+                window.location.reload();
+            }            
         });
     })
 })
