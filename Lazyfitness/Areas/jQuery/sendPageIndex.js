83abd4ddf89e8effe26ae7d057fe67abfda4b834
Kaimar666 <812758481@qq.com>
2018-11-05 17:40:59 +0800

    添加了旭哥的文件

---
+++ Lazyfitness/Areas/jQuery/sendPageIndex.js
@@ -0,0 +1,20 @@
+﻿
+
+$(function () {
+    $(".id").click(function () {
+        var id = $(this).val();
+        alert(id);
+        //$.get("/backStage/userManagement/Index", "pageIndex = " + pageIndex, function (success) {
+        //    if (success == "success") {
+        //        //刷新当前页面
+        //        window.location.reload();
+        //    }
+        //})
+        $.ajax({
+            url: '/backStage/userManagement/Index',
+            type: 'POST',
+            data: { id: id },
+            success: function () { alert("1"); /*window.location.reload(); */}
+        })
+    })
+})
