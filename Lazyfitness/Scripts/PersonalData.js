f5fb865052b1ba9c747c32c556200635cf4968b0
Kaimar666 <812758481@qq.com>
2018-11-26 22:46:27 +0800

    个人信息 应该改好了，你们看看。

--- Lazyfitness/Scripts/PersonalData.js
+++ Lazyfitness/Scripts/PersonalData.js
@@ -2,19 +2,24 @@ function changeInput() {
     document.getElementById("button").hidden = true;
     document.getElementById("postData").hidden = false;
     document.getElementById("quit").hidden = false;
+    document.getElementById("image").hidden = false;
+    document.getElementById("postImg").hidden = false;
+    document.getElementById("btnGroup").hidden = false;
     var userName = document.getElementById("userName");
     var userName_new = userName.innerHTML;
-    userName.innerHTML = "<input type='text' id='userName' name='userName' class='input' value='" + userName_new + "'/>";
+    userName.innerHTML = "<input type='text' id='userName' name='userName' class='input' value='" + userName_new + "')/>";
 
     var userSex = document.getElementById("userSex");
     var userSex_new = userSex.innerHTML;
     if (userSex_new == '男') {
-        userSex.innerHTML = "<input type='radio' id='userSex' name='userSex' class='input' value='0' checked='checked'/>男<input type='radio' id='userSex' name='userSex' class='input' value='1' />女";
+        userSex.innerHTML = "<input type='radio' id='userSex' name='userSex' class='input' value='1' checked='checked'/>男 <input type= 'radio' id= 'userSex' name= 'userSex' class='input' value= '0' />女<input type= 'radio' id= 'userSex' name= 'userSex' class='input' value= '2' />保密";
     }
     if (userSex_new == '女') {
-        userSex.innerHTML = "<input type='radio' id='userSex' name='userSex' class='input' value='0'/>男<input type='radio' name='userSex' id='userSex' class='input' value='1' checked='checked'/>女";
+        userSex.innerHTML = "<input type='radio' id='userSex' name='userSex' class='input' value='1'/>男<input type= 'radio' name= 'userSex' id= 'userSex' class='input' value= '0' checked= 'checked' />女<input type= 'radio' id= 'userSex' name= 'userSex' class='input' value= '2' />保密";
     }
-
+    if (userSex_new == '保密') {
+        userSex.innerHTML = "<input type='radio' id='userSex' name='userSex' class='input' value='1'/>男<input type= 'radio' name= 'userSex' id= 'userSex' class='input' value= '0' />女<input type= 'radio' id= 'userSex' name= 'userSex' class='input' value= '2' checked= 'checked'/>保密";
+    }
     var userEmail = document.getElementById("userEmail");
     var userEmail_new = userEmail.innerHTML;
     userEmail.innerHTML = "<input type='text' id='userEmail' name='userEmail' class='input' value='" + userEmail_new + "'/>";
@@ -24,11 +29,7 @@ function changeInput() {
     var userAge_new = userAge.innerHTML;
     userAge.innerHTML = "<input type='text' id='userAge' name='userAge' class='input' value='" + userAge_new + "'/>";
 
-    var userintroduce = document.getElementById("userIntroduce");
-    var userintroduce_new = userintroduce.innerHTML;
-    //userintroduce.innerHTML = "<textarea rows='3' cols='20' type='text' id='userintroduce' name='userintroduce' class='input' value='" + userintroduce_new + "'/>";
-    userintroduce.innerHTML = "<textarea rows='3' cols='20' type='text' id='userintroduce' name='userintroduce' class='input'/>" + userintroduce_new + "</textarea>";
+    var userIntroduce = document.getElementById("userIntroduce");
+    var userIntroduce_new = userIntroduce.innerHTML;
+    userIntroduce.innerHTML = "<textarea rows='3' cols='20' type='text' id='userIntroduce' name='userIntroduce' class='input' />" + userIntroduce_new+"</textarea>";
 }
-
-
-
